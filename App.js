07714d004ac076c17184c949a34a19178cfb6739
Damini <daminipandey1310@gmail.com>
2024-04-23 23:14:13 +0530

    feat: grid testing

--- App.js
+++ App.js
@@ -1,28 +1,68 @@
 import "./global.css";
 import React from "react";
 import { GluestackUIProvider } from "./components/ui/gluestack-ui-provider";
-
-import { CheckIcon } from "./components/ui/icon";
-import { SafeAreaView } from "react-native";
-import {
-  Checkbox,
-  CheckboxIndicator,
-  CheckboxIcon,
-  CheckboxLabel,
-} from "./components/ui/checkbox";
-import { Icon, CloseIcon } from "./components/ui/icon";
+import { SafeAreaView, Text, View } from "react-native";
+import { Grid, GridItem } from "./components/ui/grid";
 
 export default function App() {
   return (
     <GluestackUIProvider>
       <SafeAreaView
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+        style={
+          {
+            // flex: 1,
+            // alignItems: "center",
+            // justifyContent: "center",
+          }
+        }
       >
-        <Icon as={CloseIcon} color="gray" />
+        <View className="w-[200px]">
+          <Grid
+            numColumns={4}
+            // rowSpacing={2}
+            // columnSpacing={2}
+            // spacing={5}
+            spacing={5}
+            className="flex-row bg-red-600 m-10"
+          >
+            {/* <GridItem colSpan={12} className="bg-pink-500 ">
+              <Text>1</Text>
+            </GridItem> */}
+            <GridItem colSpan={2} className="bg-pink-500">
+              <Text>2</Text>
+            </GridItem>
+            <GridItem colSpan={4} className="bg-pink-500">
+              <Text>4</Text>
+            </GridItem>
+            <GridItem colSpan={2} className="bg-pink-500">
+              <Text>2</Text>
+            </GridItem>
+            <GridItem colSpan={3} className="bg-pink-500">
+              <Text>3</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+            <GridItem className="bg-pink-500">
+              <Text>1</Text>
+            </GridItem>
+          </Grid>
+        </View>
       </SafeAreaView>
     </GluestackUIProvider>
   );
